399ab940086b6e4fea2582b61e99aaa19bc92e1f
pixosphere <pixosphere@pm.me>
2022-04-12 00:31:49 +0530

    2022-04-12 00:31:49

--- src/components/Configs.js
+++ src/components/Configs.js
@@ -2,7 +2,7 @@ const Configs = {
 	NAMESPACE: "pixosphere",
 	COUNT_API_KEY: "25ca93f4-42a2-48e5-b428-bf6c7197739d",
 	OPENSEA_ASSETS: "https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963",
-	FILESTORE_BASE: "https://firebasestorage.googleapis.com/v0/b/pixosphere.appspot.com/o",
+	FILESTORE_BASE: "https://firebasestorage.googleapis.com/v0/b/pixosphere-9116e.appspot.com/o",
 	API_BASE: "https://raw.githubusercontent.com/pixosphere/pixosphere.github.io/main/apis"
 }
 
